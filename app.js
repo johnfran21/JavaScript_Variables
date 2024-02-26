6acf4b2bc7ceb50183085e1001d041fb6216caac
John Franzese <johnfranzese21@gmail.com>
2024-02-26 10:17:01 -0600

    Java Intro

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+//Exercise 1
+const firstName = "Derrick";
+let lastName = "Rose";
+let age;
 
-// Exercise 1
+//Exercise 2
+// let firstName = "Derrick";
+// let lastName = "Rose";
+age = 35;
 
-// Exercise 2
+//Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
